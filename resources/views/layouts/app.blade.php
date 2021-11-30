ad11bb887456d269aba2d6ce3abc62bd3fcafffe
henrichile <Enrique.tapia@correoaiep.cl>
2021-11-29 22:52:17 -0300

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/abastible.css') }}">
+    <link rel="stylesheet" href="{{ asset('abastible.css') }}">
 
     <title>Home - Dúo - Abastible</title>
   </head>
