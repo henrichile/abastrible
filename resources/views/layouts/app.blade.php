6dc34c2e8c54c2a2547c8fe3d89b40e757cdace1
henrichile <Enrique.tapia@correoaiep.cl>
2021-11-29 22:48:34 -0300

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,72 +11,38 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/abastible.css') }}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.4/trix.css" integrity="sha512-qjOt5KmyILqcOoRJXb9TguLjMgTLZEgROMxPlf1KuScz0ZMovl0Vp8dnn9bD5dy3CcHW5im+z5gZCKgYek9MPA==" crossorigin="anonymous" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.4/trix.js" integrity="sha512-zEL66hBfEMpJUz7lHU3mGoOg12801oJbAfye4mqHxAbI0TTyTePOOb2GFBCsyrKI05UftK2yR5qqfSh+tDRr4Q==" crossorigin="anonymous"></script>
+    <title>Home - Dúo - Abastible</title>
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <header>
+      <div class="col-md-12 cabecera-duo">
+        <div class="container">
+          <img src="{{ asset('img/logo-abastible.png') }}" alt="abastible"> 
+        </div>
+      </div>
+      <div class="col-md-12 barra-duo">
+        <div class="container">info + salir</div>
+      </div>
+    </header>
+    @yield('content')
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-xs-12">
+          <img src="{{ asset('img/logo-abastible.png') }}" alt="abastible"> 
+        </div>
+        <div class="col-md-6 col-xs-12">
+          <a class="wtsp-footer-duo" href="#">Asistente Virtual FN +56 944779661 <i clas="fa fa-whatsapp"></i></a>
+        </div>
+      </div>
     </div>
-</body>
-</html>
+  </footer>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,106 +1,45 @@
 @extends('layouts.front2')
 
 @section('content')
-    <section class="position-relative cover-background tz-builder-bg-image border-none hero-style2" data-img-size="(W)1920px X (H)1000px" style="background:linear-gradient(rgba(0,0,0,0.01), rgba(0,0,0,0.01)), url('{{ asset('images/uploads/fondoconecta3.jpg') }}')">
-        <div class="container one-fourth-screen  position-relative">
-            <div class="row">
-                <div class="slider-typography xs-position-static text-center">
-                    <div class="slider-text-middle-main">
-                       
-                        <div class="slider-text-bottom text-center">
-                            <!-- image -->
-                            <div class="col-lg-10 col-md-8 col-sm-12 col-xs-12 center-col ">
-                                <a href="{{ url('auth/google') }}"><img class="img100" src="{{ asset('images/uploads/portada_ranking.png') }}" alt=""></a>
-                            </div>
-                            <!-- end image -->
-                        </div>                                
-                    </div>
-                </div>
-            </div>
+<div class="col-md-12 banner-login">
+    <div class="row">
+      <div class="col-md-3">&nbsp;</div>
+      <div class="col-md-6 col-xs-12">
+        <div class="col-xs-12 logo-duo-home">
+          <img src="img/logo-duo.png" alt="duo">
+          <h1>¡¡ JUEGA Y GANA INCREÍBLES PREMIOS CON DÚO !!</h1>
         </div>
-    </section>  
-    <section class="bg-white builder-bg padding-110px-tb xs-padding-60px-tb cover-background" id="VIDEO"  style="background:linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.8)), url('{{ asset('images/uploads/fondoazul.png') }}')">
-        <div class="container" id="BASES">
-                     <div class="row equalize xs-equalize-auto">
-                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 display-table xs-margin-fifteen-bottom" style="height: 318px;">
-                             <div class="display-table-cell-vertical-middle">  
-                                 <!-- video -->
-                                    <div class="section-title-small sm-title-medium xs-title-medium alt-font text-center font-weight-600 text-white margin-fourteen-bottom sm-margin-twelve-bottom tz-text">VIDEO TRIVIA 3.</div>
-                                    <div class="video-overlay">
-                                       <iframe src="https://player.vimeo.com/video/539432302" width="560" height="315" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-                                     </div>
-                                 <!-- end video -->
-                             </div>
-                         </div>
-                         <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 col-lg-offset-1 display-table" style="height: 318px;">
-                            <!-- section title -->
-                            <div class="display-table-cell-vertical-middle">
-                                <h2 class="text-white title-extra-large alt-font font-weight-400 line-height-40 sm-title-large xs-title-large margin-seven-bottom sm-margin-ten-bottom tz-text">Bienvenidos al Desafío Conecta, ¡Aprende y Gana!</h2>
-                                <div class="text-white title-small sm-title-medium xs-title-medium width-90 sm-width-100 font-weight-300 margin-fourteen-bottom sm-margin-twelve-bottom tz-text">El nuevo concurso donde pondrás a prueba tus conocimientos de SAP y así ganar premios sorpresa individuales y grupales.
-<br/>¡Estudia... que ya comenzó el desafío!</div>
-              <a class="btn btn-medium propClone text-white text-white" style="background-color: rgb(146, 29, 113)" href="{{ asset('pdf/bases-desafio-conecta-final.pdf') }}" target="_blank"><span class="tz-text">DESCARGA LAS BASES</span></a>
-                            </div>
-                            <!-- end section title -->
-              
-                        </div> 
-                       </div>
+        <div class="col-xs-12">
+          <h3>INGRESA CON TU USUARIO DÚO</h3>
+          <form action="">
+            <input type="text" name="" class="" placeholder="nombre.apellido@distribuidora.cl">
+          </br>
 
-                     </div>
-                 
-     </section>
-      <section class="bg-white builder-bg padding-110px-tb xs-padding-60px-tb cover-background" id="VIDEO"  style="background:linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.8)), url('{{ asset('images/uploads/fondoazul.png') }}')">
- 
-    <div class="container">
-        <div class="row equalize xs-equalize-auto">
-            <div class="col-md-12 col-sm-12 col-xs-12 text-center">
-                    <h2 class="section-title-large sm-section-title-medium text-white font-weight-600 alt-font margin-ten-bottom xs-margin-fifteen-bottom tz-text">VIDEOS ANTERIORES</h2>
-                     
-                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 display-table xs-margin-fifteen-bottom">
-                             <div class="display-table-cell-vertical-middle">  
-                                 <!-- video -->
-                                    <div class="section-title-small sm-title-medium xs-title-medium alt-font text-center font-weight-600 text-white margin-fourteen-bottom sm-margin-twelve-bottom tz-text">VIDEO TRIVIA 2</div>
-                                    <div class="video-overlay">
-                                       <iframe src="https://player.vimeo.com/video/537263311" width="560" height="315" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-                                     </div>
-                                 <!-- end video -->
-                             </div>
-                         </div>
+          <input type="submit" value="JUGAR" class="btn_duo">
 
-
-
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 display-table xs-margin-fifteen-bottom">
-                             <div class="display-table-cell-vertical-middle">
-               
-                                 <!-- video -->
-                                 <div class="section-title-small sm-title-medium xs-title-medium alt-font font-weight-600 text-white margin-fourteen-bottom sm-margin-twelve-bottom tz-text">VIDEO TRIVIA 1</div>
-                                <div class="video-overlay">
-                                     <iframe width="560" height="315" src="https://player.vimeo.com/video/536790595" allowfullscreen=""></iframe>
-                                 </div><br/>
-                 
-                 
-                                 <!-- end video -->
-                             </div>
-                         </div>
-                         <!-- section title -->
-                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12  display-table xs-margin-fifteen-bottom">
-                              <div class="display-table-cell-vertical-middle">
-                                 <!-- video -->
-                                <div class="section-title-small sm-title-medium xs-title-medium alt-font font-weight-600 text-white margin-fourteen-bottom sm-margin-twelve-bottom tz-text">VIDEO INVITACIONAL</div>
-                                 <div class="video-overlay">
-                                     <iframe width="560" height="315" src="https://player.vimeo.com/video/534945848" allowfullscreen=""></iframe>
-                                 </div>
-                 
-                                 <!-- end video -->
-                             </div>
-                         </div>
-            </div> <!-- end section title -->
-        </div>
+        </form>
+      </div>
     </div>
+    <div class="col-md-3">&nbsp;</div>
+  </div>
+</div>
+<div class="col-md-12 info-home">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-xs-12">VIDEO</div>
+      <div class="col-md-6 col-xs-12">
+        <h1>VIDEO PRESENTACIÓN CONCURSO DÚO</h1>
+        <h3>Bienvenidos a DÚO, en el siguiente video podrás saber como participar y ganar increíbles premios junto a Peter Veneno.</h3>
+        <h3>Ingresa, usa y gana, DÚO, así mejoramos todos</h3>
+      </div>
+    </div>
+  </div>
+</div>
  
      </section>
     <script>
     // Set the date we're counting down to
-    var countDownDate = new Date("Apr 21, 2021 08:30:00").getTime();
+  /*  var countDownDate = new Date("Apr 21, 2021 08:30:00").getTime();
     
     // Update the count down every 1 second
     var x = setInterval(function() {
@@ -125,6 +64,6 @@
       if (distance < 0) {
         clearInterval(x);
       }
-    }, 1000);
+    }, 1000);*/
     </script>
     @endsection
